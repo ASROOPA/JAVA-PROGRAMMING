f0e97c6e108cd557c0dec69c8e270976046f4e07
ASROOPA <34640686+ASROOPA@users.noreply.github.com>
2018-02-08 09:58:39 +0530

    Create ArrayException.java

---
+++ ArrayException.java
@@ -0,0 +1,17 @@
+package packages;
+
+public class Exceptions {
+	public static void main(String[] args) {
+		 
+		  String languages[] = { "C", "C++", "Java", "Perl", "Python" };
+		 
+		  try {
+		    for (int c = 1; c <=5; c++) {
+		      System.out.println(languages[c]);
+		    }
+		  }
+		  catch (Exception e) {
+		    System.out.println(e);
+		  }
+		  }
+}
